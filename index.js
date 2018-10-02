32e74adedb073bf426d9d065eca09a29352a9e81
mjethvar8 <mjethvar8@gmail.com>
2018-10-02 17:04:02 -0400

    third commit

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const app = require("./server/server");
 
-module.exports = app;
+app.listen(process.env.PORT || 4000, function() {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
+});
